d1df9d397fef87e4072811aec57f205d2c101076
anant1711 <anakota9962@gmail.com>
2025-10-24 16:42:00 +0530

    Fix GitHub Pages deployment configuration

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/slixoWeb">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
