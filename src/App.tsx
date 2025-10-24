da782d4ece1c6b4af1377ee084511e05c863969e
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2025-10-24 18:52:26 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/slixoWeb">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
